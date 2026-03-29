02937f492ede472129ad82168263ecbfcffeae27
sanjay <sanjaysinghp07@gmail.com>
2026-03-29 20:52:21 +0530

    Added STATIC_ROOT

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -124,6 +124,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
         BASE_DIR / 'static',
 ]
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
